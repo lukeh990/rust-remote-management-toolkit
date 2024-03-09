546f8215ad5a63713963c8fdbd7109ced7681dfc
Luke Harding <lukeharding990@gmail.com>
2024-03-09 15:24:05 -0500

    Server now responds

--- src/bin/remote-client.rs
+++ src/bin/remote-client.rs
@@ -1,25 +1,32 @@
 use std::net::SocketAddr;
-use std::ops::Mul;
+use std::ops::MulAssign;
 use std::str::FromStr;
 use std::time::Duration;
-use tokio::io::AsyncWriteExt;
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::time::sleep;
+
 use rrmt_lib::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Setup debug variable
-    let mut debug = false;
+/*    let mut debug = false;
     #[cfg(debug_assertions)]
     {
         debug = true;
     }
+*/
 
-    let addr = SocketAddr::from_str("127.0.0.1:6000")?;
+    let addr = SocketAddr::from_str("127.0.0.1:3000")?;
     let mut stream = establish_stream_backoff(addr).await;
 
-    stream.write_all(&[0x01]).await?;
+    stream.write_all(&[0x01, 0x02, 0x03, 0x04]).await?;
+
+    let mut buffer = [0; 1];
+    let n = stream.read(&mut buffer).await?;
+    println!("Received: {:X?}", &buffer[..n]);
 
     Ok(())
 }
@@ -33,7 +40,7 @@ async fn establish_stream_backoff(addr: SocketAddr) -> TcpStream {
                 return stream;
             },
             Err(_) => {
-                backoff.mul(2);
+                backoff.mul_assign(2);
                 println!("Failed to connect retrying after {} seconds", backoff.as_secs());
                 continue;
             },
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,12 @@
-use tokio::io::AsyncReadExt;
+use std::time::Duration;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::time::sleep;
 use rrmt_lib::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let addr = "0.0.0.0:6000";
+    let addr = "0.0.0.0:3000";
     let listener = TcpListener::bind(addr).await?;
     println!("RRMT Server is now listening at: {}", addr);
 
@@ -18,17 +20,20 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn process(socket: TcpStream) -> Result<()> {
+async fn process(mut socket: TcpStream) -> Result<()> {
     let remote_ip = socket.peer_addr()?;
     println!("connection from: {}", remote_ip.ip().to_string());
 
-    let rrmt_type = determine_type(socket).await?;
+    let rrmt_type = determine_type(&mut socket).await?;
     println!("{:X?}", rrmt_type);
+
+    let buffer = &[0x0A];
+    socket.write_all(buffer).await?;
 
     Ok(())
 }
 
-async fn determine_type(mut socket: TcpStream) -> Result<u8> {
+async fn determine_type(socket: &mut TcpStream) -> Result<u8> {
     let mut buffer = [0; 1];
 
     socket.read_exact(&mut buffer[..]).await?;
